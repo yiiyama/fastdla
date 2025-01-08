d55447b1c010c4c4301d8a791bf01cff21986b0e
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-01-08 15:15:36 +0900

    Using CSC-like structure instead of list of arrays for a factor-4 speedup in _is_independent

--- fastdla/dla.py
+++ fastdla/dla.py
@@ -22,28 +22,41 @@ def _spv_commutator_norm(
 
 
 @njit
-def _compute_xmatrix(basis_indices, basis_coeffs):
-    size = len(basis_indices)
+def _compute_xmatrix(basis_indices, basis_coeffs, basis_indptr):
+    size = len(basis_indptr) - 1
     xmat = np.eye(size, dtype=np.complex128) * 0.5
-    for i1, (indices1, coeffs1) in enumerate(zip(basis_indices, basis_coeffs)):
-        for i2, (indices2, coeffs2) in enumerate(zip(basis_indices[:i1], basis_coeffs[:i1])):
-            xmat[i1, i2] = _spv_innerprod_fast(indices1, coeffs1, indices2, coeffs2)
+    for ib1 in range(size):
+        start1 = basis_indptr[ib1]
+        end1 = basis_indptr[ib1 + 1]
+        for ib2 in range(ib1):
+            start2 = basis_indptr[ib2]
+            end2 = basis_indptr[ib2 + 1]
+            xmat[ib1, ib2] = _spv_innerprod_fast(basis_indices[start1:end1],
+                                                 basis_coeffs[start1:end1],
+                                                 basis_indices[start2:end2],
+                                                 basis_coeffs[start2:end2])
 
     xmat += xmat.T.conjugate()
     return xmat
 
 
 def compute_xmatrix(basis: Sequence[SparsePauliVector]) -> np.ndarray:
-    return _compute_xmatrix([op.indices for op in basis], [op.coeffs for op in basis])
+    basis_indices = np.concatenate([op.indices for op in basis])
+    basis_coeffs = np.concatenate([op.coeffs for op in basis])
+    basis_indptr = np.cumsum([0] + [op.num_terms for op in basis])
+    return _compute_xmatrix(basis_indices, basis_coeffs, basis_indptr)
 
 
 @njit
-def _extend_xmatrix(xmat, basis_indices, basis_coeffs, new_indices, new_coeffs):
+def _extend_xmatrix(xmat, new_indices, new_coeffs, basis_indices, basis_coeffs, basis_indptr):
     current_size = xmat.shape[0]
     new_xmat = np.empty((current_size + 1, current_size + 1), dtype=np.complex128)
     new_xmat[:current_size, :current_size] = xmat
     for ib in range(current_size):
-        ip = _spv_innerprod_fast(basis_indices[ib], basis_coeffs[ib], new_indices, new_coeffs)
+        start = basis_indptr[ib]
+        end = basis_indptr[ib + 1]
+        ip = _spv_innerprod_fast(basis_indices[start:end], basis_coeffs[start:end], new_indices,
+                                 new_coeffs)
         new_xmat[ib, -1] = ip
         new_xmat[-1, ib] = ip.conjugate()
     new_xmat[-1, -1] = 1.
@@ -52,20 +65,26 @@ def _extend_xmatrix(xmat, basis_indices, basis_coeffs, new_indices, new_coeffs):
 
 def extend_xmatrix(
     xmat: np.ndarray,
-    basis: Sequence[SparsePauliVector],
-    new_op: SparsePauliVector
+    new_op: SparsePauliVector,
+    basis: Sequence[SparsePauliVector]
 ) -> np.ndarray:
-    return _extend_xmatrix(xmat, [op.indices for op in basis], [op.coeffs for op in basis],
-                           new_op.indices, new_op.coeffs)
+    basis_indices = np.concatenate([op.indices for op in basis])
+    basis_coeffs = np.concatenate([op.coeffs for op in basis])
+    basis_indptr = np.cumsum([0] + [op.num_terms for op in basis])
+    return _extend_xmatrix(xmat, new_op.indices, new_op.coeffs, basis_indices, basis_coeffs,
+                           basis_indptr)
 
 
 @njit
-def _is_independent(new_indices, new_coeffs, basis_indices, basis_coeffs, xmat_inv):
-    basis_size = len(basis_indices)
+def _is_independent(new_indices, new_coeffs, basis_indices, basis_coeffs, basis_indptr, xmat_inv):
+    basis_size = len(basis_indptr) - 1
     pidag_q = np.empty(basis_size, dtype=np.complex128)
     is_zero = True
-    for ib, (bindex, bcoeff) in enumerate(zip(basis_indices, basis_coeffs)):
-        ip = _spv_innerprod_fast(bindex, bcoeff, new_indices, new_coeffs)
+    for ib in range(basis_size):
+        start = basis_indptr[ib]
+        end = basis_indptr[ib + 1]
+        ip = _spv_innerprod_fast(basis_indices[start:end], basis_coeffs[start:end], new_indices,
+                                 new_coeffs)
         pidag_q[ib] = ip
         is_zero &= np.isclose(ip.real, 0.) and np.isclose(ip.imag, 0.)
 
@@ -75,11 +94,15 @@ def _is_independent(new_indices, new_coeffs, basis_indices, basis_coeffs, xmat_i
 
     # Residual calculation: subtract Pi*ai from Q directly
     a_proj = xmat_inv @ pidag_q
-    for a_val, bindices, bcoeffs in zip(a_proj, basis_indices, basis_coeffs):
+    for ib in range(basis_size):
+        a_val = a_proj[ib]
         if np.isclose(a_val.real, 0.) and np.isclose(a_val.imag, 0.):
             continue
+        start = basis_indptr[ib]
+        end = basis_indptr[ib + 1]
         new_indices, new_coeffs = _spv_sum_fast(new_indices, new_coeffs,
-                                                bindices, -a_val * bcoeffs)
+                                                basis_indices[start:end],
+                                                -a_val * basis_coeffs[start:end])
 
     return new_indices.shape[0] != 0
 
@@ -107,23 +130,31 @@ def is_independent(
     if xmat_inv is None:
         xmat_inv = np.linalg.inv(compute_xmatrix(basis))
 
-    return _is_independent(new_op.indices, new_op.coeffs,
-                           [op.indices for op in basis], [op.coeffs for op in basis],
+    basis_indices = np.concatenate([op.indices for op in basis])
+    basis_coeffs = np.concatenate([op.coeffs for op in basis])
+    basis_indptr = np.cumsum([0] + [op.num_terms for op in basis])
+    return _is_independent(new_op.indices, new_op.coeffs, basis_indices, basis_coeffs, basis_indptr,
                            xmat_inv)
 
 
 @njit
-def _main_loop(basis_indices, basis_coeffs, xmat_ref, result_indices, result_coeffs, verbosity):
+def _main_loop(
+    result_indices,
+    result_coeffs,
+    basis_indices,
+    basis_coeffs,
+    basis_indptr,
+    xmat,
+    verbosity
+):
     num_results = len(result_indices)
-    new_ops = []
-    xmat = xmat_ref[0]
     xmat_inv = np.linalg.inv(xmat)
     prev_indices = np.array([-1], dtype=np.uint64)
     prev_coeffs_conj = None
 
     for ires in range(num_results):
         if verbosity > 2 and ires % 100 == 0:
-            dla_dim = len(basis_indices)
+            dla_dim = len(basis_indptr) - 1
             with objmode():
                 print(f'Processing SPV {ires}/{num_results} (DLA dim {dla_dim})..', flush=True)
 
@@ -139,34 +170,43 @@ def _main_loop(basis_indices, basis_coeffs, xmat_ref, result_indices, result_coe
         prev_indices = indices
         prev_coeffs_conj = coeffs.conjugate()
 
-        if _is_independent(indices, coeffs, basis_indices, basis_coeffs, xmat_inv):
-            new_ops.append((indices, coeffs, len(basis_indices)))
-            xmat = _extend_xmatrix(xmat, basis_indices, basis_coeffs, indices, coeffs)
+        if _is_independent(indices, coeffs, basis_indices, basis_coeffs, basis_indptr, xmat_inv):
+            xmat = _extend_xmatrix(xmat, indices, coeffs, basis_indices, basis_coeffs, basis_indptr)
             xmat_inv = np.linalg.inv(xmat)
-            basis_indices.append(indices)
-            basis_coeffs.append(coeffs)
+            basis_indptr = np.append(basis_indptr, basis_indptr[-1] + indices.shape[0])
+            basis_indices = np.concatenate((basis_indices, indices))
+            basis_coeffs = np.concatenate((basis_coeffs, coeffs))
 
-    xmat_ref[0] = xmat
-    return new_ops
+    return basis_indices, basis_coeffs, basis_indptr, xmat
 
 
 def generate_dla(
     generators: Sequence[SparsePauliVector],
     *,
+    max_dim: Optional[int] = None,
     max_workers: Optional[int] = None,
     verbosity: int = 0
 ) -> list[SparsePauliVector]:
-    basis_indices = [op.indices for op in generators]
-    basis_coeffs = [op.coeffs / np.sqrt(np.sum(np.square(np.abs(op.coeffs)))) for op in generators]
+    basis_indices = np.concatenate([op.indices for op in generators])
+    basis_coeffs = np.concatenate([op.coeffs / np.sqrt(np.sum(np.square(np.abs(op.coeffs))))
+                                   for op in generators])
+    basis_indptr = np.cumsum([0] + [op.num_terms for op in generators])
     num_qubits = generators[0].num_qubits
 
-    xmat_ref = [_compute_xmatrix(basis_indices, basis_coeffs)]
+    xmat = _compute_xmatrix(basis_indices, basis_coeffs, basis_indptr)
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         commutators = {
-            executor.submit(_spv_commutator_norm, idx1, c1, idx2, c2, num_qubits)
-            for i1, (idx1, c1) in enumerate(zip(basis_indices, basis_coeffs))
-            for idx2, c2 in zip(basis_indices[:i1], basis_coeffs[:i1])
+            executor.submit(
+                _spv_commutator_norm,
+                basis_indices[start1:end1],
+                basis_coeffs[start1:end1],
+                basis_indices[start2:end2],
+                basis_coeffs[start2:end2],
+                num_qubits
+            )
+            for i1, (start1, end1) in enumerate(zip(basis_indptr[:-1], basis_indptr[1:]))
+            for start2, end2 in zip(basis_indptr[:i1], basis_indptr[1:i1 + 1])
         }
 
         if verbosity > 1:
@@ -191,25 +231,53 @@ def generate_dla(
                 print(f'Sorted in {time.time() - sort_start:.2f}s')
 
             main_loop_start = time.time()
-            new_ops = _main_loop(basis_indices, basis_coeffs, xmat_ref, result_indices,
-                                 result_coeffs, verbosity)
+            old_dim = basis_indptr.shape[0] - 1
+            basis_indices, basis_coeffs, basis_indptr, xmat = _main_loop(
+                result_indices, result_coeffs, basis_indices, basis_coeffs, basis_indptr, xmat,
+                verbosity
+            )
+            new_dim = basis_indptr.shape[0] - 1
             if verbosity > 2:
-                print(f'Found {len(new_ops)} new ops in {time.time() - main_loop_start:.2f}s')
+                print(f'Found {new_dim - old_dim} new ops in {time.time() - main_loop_start:.2f}s')
 
-            new_commutators = [
-                executor.submit(_spv_commutator_norm, indices, coeffs, idx, c, num_qubits)
-                for indices, coeffs, basis_upto in new_ops
-                for idx, c in zip(basis_indices[:basis_upto], basis_coeffs[:basis_upto])
-            ]
+            new_commutators = []
+            for ib1 in range(old_dim, new_dim):
+                start1, end1 = basis_indptr[ib1:ib1 + 2]
+                for ib2 in range(ib1):
+                    start2, end2 = basis_indptr[ib2:ib2 + 2]
+                    new_commutators.append(
+                        executor.submit(
+                            _spv_commutator_norm,
+                            basis_indices[start1:end1],
+                            basis_coeffs[start1:end1],
+                            basis_indices[start2:end2],
+                            basis_coeffs[start2:end2],
+                            num_qubits
+                        )
+                    )
             commutators.update(new_commutators)
-            if verbosity > 1:
+            if verbosity > 1 and new_commutators:
                 print(f'Adding {len(new_commutators)} commutators; total {len(commutators)}')
-            if new_ops and verbosity > 0:
-                print(f'Current DLA dimension: {len(basis_indices)}')
+            if verbosity > 0:
+                print(f'Current DLA dimension: {new_dim}')
             if verbosity > 2:
                 print(f'Outer loop took {time.time() - outer_loop_start:.2f}s')
 
+            if max_dim is not None and new_dim >= max_dim:
+                basis_indptr = basis_indptr[:max_dim + 1]
+                basis_indices = basis_indices[:basis_indptr[-1]]
+                basis_coeffs = basis_coeffs[:basis_indptr[-1]]
+                executor.shutdown(wait=False, cancel_futures=True)
+                break
+
             done, _ = wait(commutators, return_when=FIRST_COMPLETED)
 
-    return [SparsePauliVector(idx, c, num_qubits=num_qubits, no_check=True)
-            for idx, c in zip(basis_indices, basis_coeffs)]
+    return [
+        SparsePauliVector(
+            basis_indices[start:end],
+            basis_coeffs[start:end],
+            num_qubits=num_qubits,
+            no_check=True
+        )
+        for start, end in zip(basis_indptr[:-1], basis_indptr[1:])
+    ]
