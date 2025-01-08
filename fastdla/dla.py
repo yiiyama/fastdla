37dbc1bf3cb1a74c3c73ec1b09d50dc56506e0f4
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-01-08 13:06:51 +0900

    Added print statements; unpacked indices and coeffs because numba was performing some transformation on list of tuples

--- fastdla/dla.py
+++ fastdla/dla.py
@@ -1,11 +1,24 @@
 """Calculation of DLA."""
 from collections.abc import Sequence
-from concurrent.futures import FIRST_COMPLETED, ThreadPoolExecutor, wait
+from concurrent.futures import ALL_COMPLETED, FIRST_COMPLETED, ThreadPoolExecutor, wait
 from typing import Optional
+import time
 import numpy as np
-from numba import njit
+from numba import njit, objmode
 from .sparse_pauli_vector import SparsePauliVector
 from .spv_fast import _spv_commutator_fast, _spv_innerprod_fast, _spv_sum_fast
+
+
+@njit(nogil=True)
+def _spv_commutator_norm(
+    indices1: np.ndarray,
+    coeffs1: np.ndarray,
+    indices2: np.ndarray,
+    coeffs2: np.ndarray,
+    num_qubits: int
+) -> tuple[np.ndarray, np.ndarray]:
+    indices, coeffs = _spv_commutator_fast(indices1, coeffs1, indices2, coeffs2, num_qubits)
+    return indices, coeffs / np.sqrt(np.sum(np.square(np.abs(coeffs))))
 
 
 @njit
@@ -100,15 +113,25 @@ def is_independent(
 
 
 @njit
-def _main_loop(basis_indices, basis_coeffs, xmat_cont, results_sorted):
+def _main_loop(basis_indices, basis_coeffs, xmat_ref, result_indices, result_coeffs, verbosity):
+    num_results = len(result_indices)
     new_ops = []
-    xmat_inv = np.linalg.inv(xmat_cont[0])
+    xmat = xmat_ref[0]
+    xmat_inv = np.linalg.inv(xmat)
     prev_indices = np.array([-1], dtype=np.uint64)
     prev_coeffs_conj = None
-    for indices, coeffs in results_sorted:
+
+    for ires in range(num_results):
+        if verbosity > 2 and ires % 100 == 0:
+            with objmode():
+                print(f'Processing SPV {ires}/{num_results} (DLA dimension {len(basis_indices)})..',
+                      flush=True)
+
+        indices = result_indices[ires]
         if indices.shape[0] == 0:
             continue
-        coeffs /= np.sqrt(np.sum(np.square(np.abs(coeffs))))
+
+        coeffs = result_coeffs[ires]
         if (prev_indices.shape[0] == indices.shape[0] and np.all(prev_indices == indices)
                 and np.isclose(np.abs(prev_coeffs_conj @ coeffs), 1.)):
             continue
@@ -116,57 +139,65 @@ def _main_loop(basis_indices, basis_coeffs, xmat_cont, results_sorted):
         prev_indices = indices
         prev_coeffs_conj = coeffs.conjugate()
 
-        if not _is_independent(indices, coeffs, basis_indices, basis_coeffs, xmat_inv):
-            continue
+        if _is_independent(indices, coeffs, basis_indices, basis_coeffs, xmat_inv):
+            new_ops.append((indices, coeffs, len(basis_indices)))
+            xmat = _extend_xmatrix(xmat, basis_indices, basis_coeffs, indices, coeffs)
+            xmat_inv = np.linalg.inv(xmat)
+            basis_indices.append(indices)
+            basis_coeffs.append(coeffs)
 
-        new_ops.append((indices, coeffs, len(basis_indices)))
-
-        xmat_cont[0] = _extend_xmatrix(xmat_cont[0], basis_indices, basis_coeffs, indices, coeffs)
-        xmat_inv = np.linalg.inv(xmat_cont[0])
-        basis_indices.append(indices)
-        basis_coeffs.append(coeffs)
-
+    xmat_ref[0] = xmat
     return new_ops
 
 
 def generate_dla(
     generators: Sequence[SparsePauliVector],
     *,
-    verbosity=0
+    max_workers: Optional[int] = None,
+    verbosity: int = 0
 ) -> list[SparsePauliVector]:
     basis_indices = [op.indices for op in generators]
     basis_coeffs = [op.coeffs / np.sqrt(np.sum(np.square(np.abs(op.coeffs)))) for op in generators]
     num_qubits = generators[0].num_qubits
 
-    xmat_cont = [_compute_xmatrix(basis_indices, basis_coeffs)]
+    xmat_ref = [_compute_xmatrix(basis_indices, basis_coeffs)]
 
-    with ThreadPoolExecutor() as executor:
-        commutators = set([
-            executor.submit(_spv_commutator_fast, idx1, c1, idx2, c2, num_qubits)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        commutators = {
+            executor.submit(_spv_commutator_norm, idx1, c1, idx2, c2, num_qubits)
             for i1, (idx1, c1) in enumerate(zip(basis_indices, basis_coeffs))
             for idx2, c2 in zip(basis_indices[:i1], basis_coeffs[:i1])
-        ])
+        }
+
         if verbosity > 1:
             print(f'Starting with {len(commutators)} commutators..')
 
+        done, _ = wait(commutators, return_when=ALL_COMPLETED)
+
         while commutators:
-            done, _ = wait(commutators, return_when=FIRST_COMPLETED)
+            outer_loop_start = time.time()
             if verbosity > 1:
-                print(f'Evaluating {len(done)} commutators; total {len(commutators)}')
+                print(f'Evaluating {len(done)}/{len(commutators)} commutators for independence')
 
             commutators.difference_update(done)
-            if verbosity > 1:
-                print(f'{len(commutators)} commutators remain unevaluated')
-
             results = [fut.result() for fut in done]
+
+            sort_start = time.time()
             indices_tuples = [tuple(res[0]) for res in results]
             sort_idx = sorted(range(len(results)), key=indices_tuples.__getitem__)
-            results = [results[idx] for idx in sort_idx]
+            result_indices = [results[idx][0] for idx in sort_idx]
+            result_coeffs = [results[idx][1] for idx in sort_idx]
+            if verbosity > 2:
+                print(f'Sorted in {time.time() - sort_start:.2f}s')
 
-            new_ops = _main_loop(basis_indices, basis_coeffs, xmat_cont, results)
+            main_loop_start = time.time()
+            new_ops = _main_loop(basis_indices, basis_coeffs, xmat_ref, result_indices,
+                                 result_coeffs, verbosity)
+            if verbosity > 2:
+                print(f'Found {len(new_ops)} new ops in {time.time() - main_loop_start:.2f}s')
 
             new_commutators = [
-                executor.submit(_spv_commutator_fast, indices, coeffs, idx, c, num_qubits)
+                executor.submit(_spv_commutator_norm, indices, coeffs, idx, c, num_qubits)
                 for indices, coeffs, basis_upto in new_ops
                 for idx, c in zip(basis_indices[:basis_upto], basis_coeffs[:basis_upto])
             ]
@@ -175,6 +206,10 @@ def generate_dla(
                 print(f'Adding {len(new_commutators)} commutators; total {len(commutators)}')
             if new_ops and verbosity > 0:
                 print(f'Current DLA dimension: {len(basis_indices)}')
+            if verbosity > 2:
+                print(f'Outer loop took {time.time() - outer_loop_start:.2f}s')
+
+            done, _ = wait(commutators, return_when=FIRST_COMPLETED)
 
     return [SparsePauliVector(idx, c, num_qubits=num_qubits, no_check=True)
             for idx, c in zip(basis_indices, basis_coeffs)]
