fdf7e9ed9165755e209f7b0eb2ca8b76ea720997
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-01-10 16:51:55 +0900

    Was running out of memory by pooling the commutators - actually that was not at all necessary.

--- fastdla/dla_dense.py
+++ fastdla/dla_dense.py
@@ -1,5 +1,6 @@
 """Calculation of DLA based on dense matrices."""
 from collections.abc import Sequence
+from functools import partial
 from typing import Optional
 import time
 import numpy as np
@@ -71,9 +72,17 @@ def is_independent(
     return _is_independent(new_op, jnp.asarray(basis), xmat_inv)
 
 
-@jax.jit
-def _main_loop_body(val):
-    ires, basis, size, xmat_inv, comms = val
+@partial(jax.jit, static_argnames=['verbosity'])
+def _main_loop_body(val, verbosity=0):
+    idx1, idx2, basis, size, xmat_inv = val
+
+    if verbosity > 1:
+        jax.lax.cond(
+            jnp.equal(((idx1 * (idx1 + 1)) // 2 + idx2) % 500, 0),
+            lambda: jax.debug.print('Basis size {size} commutator [b[{idx1}], b[{idx2}]]',
+                                    size=size, idx1=idx1, idx2=idx2),
+            lambda: None
+        )
 
     def _continue(_, _basis, _size, _xmat_inv):
         return _basis, _size, _xmat_inv
@@ -91,13 +100,17 @@ def _main_loop_body(val):
             _comm, _basis, _size, _xmat_inv
         )
 
-    comm = comms[ires]
-    return (ires + 1,) + jax.lax.cond(
+    comm = _commutator_norm(basis[idx1], basis[idx2])
+    return jax.lax.cond(
+        jnp.equal(idx2 + 1, idx1),
+        lambda: (idx1 + 1, 0),
+        lambda: (idx1, idx2 + 1)
+    ) + jax.lax.cond(
         jnp.allclose(comm, 0.),
         _continue,
         _if_independent_update,
         comm, basis, size, xmat_inv
-    ) + (comms,)
+    )
 
 
 def generate_dla(
@@ -113,50 +126,41 @@ def generate_dla(
 
     generators = jnp.asarray(generators)
     basis = jnp.resize(generators, (max_size,) + generators.shape[1:]).at[size:].set(0.)
+    xmat_inv = jnp.linalg.inv(_compute_xmatrix(basis))
+    idx1, idx2 = 1, 0
 
-    idx1, idx2 = np.array([[i1, i2] for i1 in range(size) for i2 in range(i1)]).T
-    comms = _vcommutator_norm(basis[idx1], basis[idx2])
-    icomm = 0
+    main_loop_body = partial(_main_loop_body, verbosity=verbosity)
 
     while True:
-        outer_loop_start = time.time()
-        if verbosity > 1:
-            print(f'Evaluating {comms[icomm:].shape[0]} commutators for independence')
-
         main_loop_start = time.time()
 
-        xmat_inv = jnp.linalg.inv(_compute_xmatrix(basis))
-        icomm, basis, new_size, _, _ = jax.lax.while_loop(
+        idx1, idx2, basis, new_size, xmat_inv = jax.lax.while_loop(
             lambda val: jnp.logical_and(
-                jnp.not_equal(val[0], comms.shape[0]),
-                jnp.not_equal(val[2], val[1].shape[0])
+                jnp.not_equal(val[0], val[3]),
+                jnp.not_equal(val[3], basis.shape[0])
             ),
-            _main_loop_body,
-            (icomm, basis, size, xmat_inv, comms)
+            main_loop_body,
+            (idx1, idx2, basis, size, xmat_inv)
         )
 
-        if verbosity > 2:
+        if verbosity > 0:
             print(f'Found {new_size - size} new ops in {time.time() - main_loop_start:.2f}s')
-
-        if icomm != comms.shape[0]:
-            # Need to resize basis and xmat
-            max_size += 1024
-            basis = jnp.resize(basis, (max_size,) + basis.shape[1:]).at[new_size:].set(0.)
-        elif new_size == size:
-            break
-        elif max_dim is not None and new_size >= max_dim:
-            basis = basis[:max_dim]
-            break
-        else:
-            idx1, idx2 = np.array([[i1, i2] for i1 in range(size, new_size) for i2 in range(i1)]).T
-            comms = _vcommutator_norm(basis[idx1], basis[idx2])
-            icomm = 0
 
         size = new_size
 
-        if verbosity > 0:
-            print(f'Current DLA dimension: {size}')
-        if verbosity > 2:
-            print(f'Outer loop took {time.time() - outer_loop_start:.2f}s')
+        if idx1 != size:
+            # Need to resize basis and xmat
+            if verbosity > 0:
+                print(f'Resizing basis array to {max_size + 1024}')
+
+            max_size += 1024
+            basis = jnp.resize(basis, (max_size,) + basis.shape[1:]).at[size:].set(0.)
+            xmat_inv = jnp.linalg.inv(_compute_xmatrix(basis))
+            continue
+
+        if max_dim is not None and size >= max_dim:
+            basis = basis[:max_dim]
+
+        break
 
     return [np.asarray(op) for op in basis[:size]]
