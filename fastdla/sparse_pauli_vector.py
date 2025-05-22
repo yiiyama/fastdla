a03749d3cf39b4ef2c67b47bf006a6b6e8c5d881
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-05-22 12:55:32 +0900

    Introduced PauliProduct class and updated matrix generation

--- fastdla/sparse_pauli_vector.py
+++ fastdla/sparse_pauli_vector.py
@@ -5,6 +5,7 @@ from typing import Optional
 import numpy as np
 from scipy.sparse import csr_array
 
+
 PAULI_NAMES = ['I', 'X', 'Y', 'Z']
 PAULI_INDICES = {'I': 0, 'X': 1, 'Y': 2, 'Z': 3}
 PAULI_PROD_INDEX = np.array([
@@ -25,20 +26,95 @@ PAULIS = np.array([
     [[0., -1.j], [1.j, 0.]],
     [[1., 0.], [0., -1.]]
 ])
+PAULI_SPARSE_COLS = np.array([
+    [0, 1],
+    [1, 0],
+    [1, 0],
+    [0, 1]
+])
+PAULI_SPARSE_DATA = np.array([
+    [1., 1.],
+    [1., 1.],
+    [-1.j, 1.j],
+    [1., -1.]
+])
+
+
+class PauliProduct:
+    """Product of Pauli operators."""
+    @staticmethod
+    def str_to_idx(pstr: str) -> int:
+        indices = np.array([PAULI_INDICES[p.upper()] for p in pstr], dtype=np.uint64)
+        return np.sum(indices * (4 ** np.arange(indices.shape[0])[::-1]))
+
+    @staticmethod
+    def seq_to_idx(arr: Sequence[int]) -> int:
+        indices = np.asarray(arr)
+        if np.any(np.logical_or(indices < 0, indices > 3)):
+            raise ValueError('Invalid Pauli index')
+        return np.sum(indices * (4 ** np.arange(indices.shape[0])))
+
+    def __init__(
+        self,
+        index: str | int | Sequence[int],
+        num_qubits: Optional[int] = None
+    ):
+        try:
+            num_qubits = len(index)
+        except TypeError as exc:
+            if num_qubits is None:
+                raise ValueError('Need num_qubits if index is an int') from exc
+            self.index = index
+        else:
+            if num_qubits == 0:
+                raise ValueError('Null sequence')
+            if isinstance(index, str):
+                try:
+                    self.index = PauliProduct.str_to_idx(index)
+                except KeyError as ex:
+                    raise ValueError('Invalid Pauli string') from ex
+            else:
+                try:
+                    self.index = PauliProduct.seq_to_idx(index)
+                except ValueError as ex:
+                    raise ValueError('Invalid Pauli index') from ex
+
+        self.num_qubits = num_qubits
+
+    def __str__(self) -> str:
+        return ''.join(PAULI_NAMES[i] for i in self.indices())
+
+    def __repr__(self) -> str:
+        return f"PauliProduct('{str(self)}')"
+
+    def indices(self) -> np.ndarray:
+        return ((self.index // (4 ** np.arange(self.num_qubits))) % 4).astype(int)
+
+    def to_matrix(self, *, sparse: bool = False, npmod=np) -> np.ndarray | csr_array:
+        if sparse:
+            cols = npmod.array(0)
+            data = npmod.array(1.)
+            for iq, ip in enumerate(self.indices()):
+                cols = npmod.add.outer(PAULI_SPARSE_COLS[ip] * (2 ** iq), cols).reshape(-1)
+                data = npmod.outer(PAULI_SPARSE_DATA[ip], data).reshape(-1)
+            dim = 2 ** self.num_qubits
+            indptr = npmod.arange(dim + 1)
+            if npmod is np:
+                return csr_array((data, cols, indptr), shape=(dim, dim))
+            else:
+                # pylint: disable-next=import-outside-toplevel
+                from jax.experimental.sparse import BCSR
+                return BCSR((data, cols, indptr), shape=(dim, dim), indices_sorted=True,
+                            unique_indices=True)
+
+        matrix = np.array(1.)
+        for ip in self.indices():
+            matrix = np.kron(PAULIS[ip], matrix)
+        return matrix
 
 
 class SparsePauliVector:
     """Purpose-specific implementation of a sparse Pauli operator."""
-    @staticmethod
-    def str_to_idx(pstr: str) -> int:
-        indices = np.array([PAULI_INDICES[p] for p in pstr], dtype=np.uint64)
-        return np.sum(indices * (4 ** np.arange(indices.shape[0])[::-1]))
-
-    @staticmethod
-    def idx_to_str(idx: int, num_qubits: int) -> str:
-        indices = ((idx // (4 ** np.arange(num_qubits)[::-1])) % 4).astype(int)
-        return ''.join(PAULI_NAMES[i] for i in indices)
-
     @classmethod
     def switch_impl(cls, to: str):
         if to == 'ref':
@@ -83,7 +159,7 @@ class SparsePauliVector:
 
         if isinstance(indices[0], str):
             num_qubits = len(indices[0])
-            indices = np.array([self.str_to_idx(pstr) for pstr in indices], dtype=np.uint64)
+            indices = np.array([PauliProduct.str_to_idx(pstr) for pstr in indices], dtype=np.uint64)
         elif num_qubits is None:
             raise ValueError('Need num_qubits')
         else:
@@ -98,7 +174,7 @@ class SparsePauliVector:
             raise ValueError('Indices and coeffs shape mismatch')
 
     def __str__(self) -> str:
-        opstr = ', '.join(f'{coeff} {self.idx_to_str(idx, self.num_qubits)}'
+        opstr = ', '.join(f'{coeff} {str(PauliProduct(idx, self.num_qubits))}'
                           for coeff, idx in zip(self.coeffs, self.indices))
         return f'[{opstr}]'
 
@@ -119,7 +195,7 @@ class SparsePauliVector:
 
     @property
     def paulis(self) -> list[str]:
-        return [self.idx_to_str(idx, self.num_qubits) for idx in self.indices]
+        return [str(PauliProduct(idx, self.num_qubits)) for idx in self.indices]
 
     @property
     def hpart(self) -> 'SparsePauliVector':
@@ -194,20 +270,19 @@ class SparsePauliVector:
         dense[self.indices] = self.coeffs
         return dense
 
-    def to_matrix(self, *, npmod=np) -> np.ndarray:
+    def to_matrix(self, *, sparse: bool = False, npmod=np) -> np.ndarray:
         """Convert the Pauli sum to a dense (2**num_qubits, 2**num_qubits) matrix.
 
         SparsePauliOp.to_matrix() seems to have a much more efficient implementation.
         """
-        pauli_indices = ((self.indices[:, None] // (4 ** np.arange(self.num_qubits)[None, ::-1]))
-                         % 4).astype(int)
-        args = ()
-        for iq in range(self.num_qubits):
-            args += (PAULIS[pauli_indices[:, iq]], [0, iq + 1, self.num_qubits + iq + 1])
-        args += (list(range(2 * self.num_qubits + 1)),)
-        matrices = npmod.einsum(*args)
-        matrices = matrices.reshape((self.num_terms, 2 ** self.num_qubits, 2 ** self.num_qubits))
-        return npmod.sum(self.coeffs[:, None, None] * matrices, axis=0)
+        matrix = (
+            PauliProduct(self.indices[0], self.num_qubits).to_matrix(sparse=sparse, npmod=npmod)
+            * self.coeffs[0]
+        )
+        for index, coeff in zip(self.indices[1:], self.coeffs[1:]):
+            matrix += (PauliProduct(index, self.num_qubits).to_matrix(sparse=sparse, npmod=npmod)
+                       * coeff)
+        return matrix
 
 
 class SparsePauliVectorArray:
