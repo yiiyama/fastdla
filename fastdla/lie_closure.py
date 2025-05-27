d01f25774eb9ca2295933af0fd24464692dd03f5
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-05-27 14:21:20 +0900

    Updated doc

--- fastdla/lie_closure.py
+++ fastdla/lie_closure.py
@@ -16,29 +16,44 @@ def linear_independence(
     basis: Basis,
     xinv: Optional[InnerProductMatrix] = None
 ) -> bool:
-    """Check if the given operator is linearly independent from all other elements in the basis.
+    r"""Check if the given operator is linearly independent from all other elements in the basis.
 
-    Let the Lie algebra elements in the basis be P0, P1, ..., Pn. The basis_matrix Π is a matrix
-    formed by stacking the column vectors {Pi}:
-    Π = (P0, P1, ..., Pn).
-    If a new element Q is linearly dependent on {Pi}, there is a column vector of coefficients
-    a = (a0, a1, ..., an)^T where
-    Π a = Q.
-    Multiply both sides with Π† and denote X = Π†Π to obtain
-    X a = Π† Q.
-    Since {Pi} are linearly independent, X must be invertible:
-    a = X^{-1} Π† Q.
-    Using thus calculated {ai}, we check the residual
-    R = Q - Π a
-    to determine the linear independence of Q with respect to {Pi}.
+    Let the Lie algebra elements in the basis :math:`B` be :math:`g_0, g_1, ..., g_{n-1}`. If a new
+    element :math:`h` is linearly dependent on :math:`\{g_j\}_{j=0}^{n-1}`, there is a column vector
+    of coefficients :math:`\mathbf{a} = (a_0, a_1, ..., a_{n-1})^T` where
+
+    .. math::
+
+        \sum_{k=0}^{n-1} g_k a_k = h.
+
+    Using the inner product between algebra elements, let :math:`X_{jk} = \langle g_j, g_k \rangle`.
+    Inner product of both sides with :math:`g_j` yields
+
+    .. math::
+
+        \sum_{k=0}^{n-1} X_{jk} a_k = \langle g_j, h \rangle.
+
+    Since :math:`B` is linearly independent, the matrix :math:`X` is invertible. Therefore
+
+    .. math::
+
+        a_j = \sum_{k=0}^{n-1} (X^{-1})_{jk} \langle g_k, h \rangle.
+
+    Using thus calculated :math:`\{a_j\}_{j=0}^{n-1}`, we check the residual
+
+    .. math::
+
+        \Delta = h - \sum_{j=0}^{n-1} g_j a_j
+
+    to determine the linear independence of :math:`h` with respect to :math:`B`.
 
     Args:
-        new_op: Lie algebra element Q to check the linear independence of.
+        new_op: Lie algebra element :math:`h` to check the linear independence of.
         basis: The basis (list of linearly independent elements) of the Lie Algebra.
-        xinv: Inverse of the X matrix. If not given, computed from basis.
+        xinv: Inverse of the :math:`X` matrix. If not given, computed from basis.
 
     Returns:
-        True if Q is linearly independent from all elements of the basis.
+        True if :math:`h` is linearly independent from all elements of the basis.
     """
     if isinstance(op, SparsePauliVector):
         from fastdla._lie_closure_impl.sparse_numba import linear_independence as fn
@@ -52,13 +67,14 @@ def orthogonalize(
     op: AlgebraElement,
     basis: Basis
 ) -> AlgebraElement:
-    """Subtract the subspace projection of an algebra element from itself.
+    r"""Subtract the subspace projection of an algebra element from itself.
 
-    Let the orthonormal basis be P0, P1, ..., Pn. The basis_matrix Π is a matrix formed by stacking
-    the column vectors {Pi}:
-    Π = (P0, P1, ..., Pn).
-    The orthogonal component of Q with respect to the basis is given by
-    T = Q - Π Π† Q.
+    Let the orthonormal basis be :math:`B = \{g_j\}_{j=0}^{n-1}`. The orthogonal component of
+    :math:`h` with respect to :math:`B` is given by
+
+    .. math::
+
+        t = h - \sum_{j=0}^{n-1} \langle g_j, h \rangle g_j.
     """
     if isinstance(op, SparsePauliVector):
         from fastdla._lie_closure_impl.sparse_numba import orthogonalize as fn
@@ -93,7 +109,7 @@ def lie_closure(
     Specific implementations employ different optimization strategies in the loop implementations.
 
     The algorithm to determine the linear independence of a_k with A is described in
-    linear_independence(). When we are allowed to modify the original generators so that A can be
+    `linear_independence`. When we are allowed to modify the original generators so that A can be
     orthonormalized, the linear independence can be determined through checking the existence of an
     orthogonal component of a_k with respect to the subspace spanned by A. If such a component
     exists, its normalized form is added to A in the update step.
@@ -104,7 +120,6 @@ def lie_closure(
             orthonormalized Lie algebra elements are kept in memory to speed up the calculation.
         max_dim: Cutoff for the dimension of the Lie closure. If set, the algorithm may be halted
             before a full closure is obtained.
-        verbosity: Verbosity level.
         min_tasks: (CPU-based implementation only) Minimum number of commutator calculations to
             complete before starting a new batch of calculations.
         max_workers: (CPU-based implementation only) Maximun number of threads to use to parallelize
